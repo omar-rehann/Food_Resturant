1fffaf706a863e5c526915e2bb99077b6fb26818
Omar Rehan <omarrehan724@gmail.com>
2026-09-09 11:09:06 +0300

    add new edit in env

--- Backend/config/supabase.js
+++ Backend/config/supabase.js
@@ -5,9 +5,5 @@ const supabase = createClient(
     process.env.SUPABASE_URL,
     process.env.SUPABASE_SECRET_KEY
 );
-console.log(
-    "SUPABASE KEY TYPE:",
-    process.env.SUPABASE_SECRET_KEY
-);
 
 export default supabase;
