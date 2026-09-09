8ed2a4e7793f8e6f6523e42fb7beae628333568c
Omar Rehan <omarrehan724@gmail.com>
2026-09-09 11:16:08 +0300

    add new edit

--- Backend/config/supabase.js
+++ Backend/config/supabase.js
@@ -5,5 +5,13 @@ const supabase = createClient(
     process.env.SUPABASE_URL,
     process.env.SUPABASE_SECRET_KEY
 );
+console.log("SUPABASE URL:", process.env.SUPABASE_URL);
+console.log(
+    "SUPABASE KEY EXISTS:", !!process.env.SUPABASE_SECRET_KEY
+);
+console.log(
+    "SUPABASE KEY PREFIX:",
+    process.env.SUPABASE_SECRET_KEY
+);
 
 export default supabase;
